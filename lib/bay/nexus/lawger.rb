af1b3a66e1bc9391f65f9272aea03f91606333df
Zsolt Fekete <zsoltf@me.com>
2015-12-23 17:23:38 -0800

    fix namespace

--- lib/bay/nexus/lawger.rb
+++ lib/bay/nexus/lawger.rb
@@ -1,10 +1,16 @@
 require 'logger'
 
-module Lawger
-  def log message
-    classname = self.class.to_s.downcase
-    filename = "/tmp/#{classname}.log"
-    @logger ||= Logger.new(filename)
-    @logger.debug message
+module Bay
+  module Nexus
+    module Lawger
+
+      def log message
+        classname = self.class.to_s.downcase.gsub!('::', '_')
+        filename = "/tmp/#{classname}.log"
+        @logger ||= Logger.new(filename)
+        @logger.debug message
+      end
+
+    end
   end
 end
--- lib/bay/nexus/summoner.rb
+++ lib/bay/nexus/summoner.rb
@@ -8,50 +8,54 @@ module Daemons
   end
 end
 
-module Summoner
+module Bay
+  module Nexus
+    module Summoner
 
-  def initialize options={}
-    init_daemon
-  end
+      def initialize options={}
+        init_daemon
+      end
 
-  def init_daemon
-    options = {
-      log_output: false,
-      backtrace: false,
-      multiple: true,
-      ARGV: ['start'],
-      show_status_callback: :customstatus
-    }
+      def init_daemon
+        options = {
+          log_output: false,
+          backtrace: false,
+          multiple: true,
+          ARGV: ['start'],
+          show_status_callback: :customstatus
+        }
 
-    name = self.class.to_s.downcase
-    @daemon = Daemons.run_proc(name, options) { start }
+        name = self.class.to_s.downcase
+        @daemon = Daemons.run_proc(name, options) { start }
 
-  end
+      end
 
-  def daemon
-    # run_proc with :multiple creates an application group (stack)
-    # if there are multiple daemons with the same name,
-    # the curent daemon is the last entry
-    @daemon.applications.last
-  end
+      def daemon
+        # run_proc with :multiple creates an application group (stack)
+        # if there are multiple daemons with the same name,
+        # the curent daemon is the last entry
+        @daemon.applications.last
+      end
 
-  def start_daemon
-    daemon.start
-  end
+      def start_daemon
+        daemon.start
+      end
 
-  def stop_daemon
-    daemon.stop
-  end
+      def stop_daemon
+        daemon.stop
+      end
 
-  def pid
-    daemon.pid.pid
-  end
+      def pid
+        daemon.pid.pid
+      end
 
-  def status
-    daemon.show_status
-  end
+      def status
+        daemon.show_status
+      end
 
-  def name
-    [daemon.pid.progname, daemon.pid.number + 1].join("-")
+      def name
+        [daemon.pid.progname, daemon.pid.number + 1].join("-")
+      end
+    end
   end
 end
