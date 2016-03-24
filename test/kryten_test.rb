15e8a855e2ed0cbda052decf1b365c6437c1ec99
Zsolt Fekete <zsoltf@me.com>
2016-03-23 21:58:22 -0700

    update test

--- test/kryten_test.rb
+++ test/kryten_test.rb
@@ -16,7 +16,7 @@ class KrytenRunnerTest < Minitest::Test
   def test_it_runs_task_once
     @t.run
     sleep 0.3
-    assert_equal Kryten::TestOutput.logs,
-      ["initializing", "working for 0.1", "done working"]
+    assert_equal Kryten::TestOutput.logs.sort,
+      ["working for 0.1", "done working"].sort
   end
 end
